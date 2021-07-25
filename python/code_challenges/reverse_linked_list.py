a4886ae646b36d047bcabb99d245c78690510aa5
FIRAS AHMAD HASAN <x.firashasan@gmail.com>
2021-07-25 23:43:13 +0300

    white board

--- python/code_challenges/reverse_linked_list.py
+++ python/code_challenges/reverse_linked_list.py
@@ -1,9 +1,14 @@
-def reverse_list(ll):
-    """Reverses a linked list
-    Args:
-        ll: linked list
-    Returns:
-        linked list in reversed form
-    """
-    # put your function implementation here
-    return ll
+# def reverse_list(ll):
+#     """Reverses a linked list
+#     Args:
+#         ll: linked list
+#     Returns:
+#         linked list in reversed form
+#     """
+#     # put your function implementation here
+#     return ll
+def reverseArray(setOfNumbers):
+    reversedNumbers=setOfNumbers[::-1]
+    return reversedNumbers
+setOfNumbers = input("enter set of numbers ")
+print("revresing your numbers: "+reverseArray(setOfNumbers))    
